5a9396b3b8faeb4cd24648ed18816351000d83f5
Paulo Silva <pauloalmeida1288@gmail.com>
2023-03-16 00:04:54 +0000

    Início do ex.1 com erro no foreach

---
+++ ChangeString/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ChangeString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("String: ");
+            string str = Console.ReadLine();
+            Console.Write("Char: ");
+            string char_ = Console.ReadLine();
+
+            foreach (string s in str)
+            {
+                if (s != char_)
+                {
+                    Console.Write(s);
+                }
+            }
+        }
+    }
+}
