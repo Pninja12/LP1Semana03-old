bc45a2efb4e3657751498bfd7208569a5143b3e5
Pninja12 <74876860+Pninja12@users.noreply.github.com>
2023-03-17 17:42:13 +0000

    Ex.3 semi-concluido, não funciona mas a lógica está toda certa(acho eu)

---
+++ PlayerPerks/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+
+namespace PlayerPerks
+{
+    class Program
+    {
+        [Flags]
+            enum Power
+            {
+                WaterBreathing = 1 << 0 , // 1
+                Stealth = 1 << 1 , // 2
+                AutoHeal = 1 << 2, // 4
+                DoubleJump = 1 << 3 //8
+            }
+        static void Main(string[] args)
+        {
+            Power mypower;
+
+            Console.Write("Say the magic word: ");
+            string str = Console.ReadLine();
+            int error = 0;
+
+            char a = "a";
+            char w = "w";
+            char s = "s";
+            char d = "d";
+
+            foreach(char s in str)//carácter por carácter da lista de "str"
+            {
+                if (s == w)
+                {
+                    mypower &= ~Power.WaterBreathing;
+                }
+                else if (s == s)
+                {
+                    mypower &= ~Power.Stealth;
+                }
+                else if (s == a)
+                {
+                    mypower &= ~Power.AutoHeal;
+                }
+                else if (s == d)
+                {
+                    mypower &= ~Power.DoubleJump;
+                }
+                else
+                {
+                    error = 1;
+                }
+
+                if (error == 1)
+                {
+                    Console.WriteLine("Unknown perk!");
+                }
+                else
+                {
+                    Console.WriteLine(mypower);
+                    if ((mypower & Power.DoubleJump) == Power.DoubleJump && (mypower & Power.Stealth) == Power.Stealth)
+                    {
+                        Console.WriteLine("Silent jumper!");
+                    }
+                    if ((mypower & Power.AutoHeal) != Power.AutoHeal)
+                    {
+                        Console.WriteLine("Not gonna make it!");
+                    }
+                    
+                }
+            }
+
+        }
+    }
+}
