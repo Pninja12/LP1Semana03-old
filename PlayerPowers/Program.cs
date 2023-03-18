a1d096ffa3d742795d254e8371e9166a08eb9b32
Pninja12 <74876860+Pninja12@users.noreply.github.com>
2023-03-18 14:20:09 +0000

    Ex.5 concluído

--- PlayerPowers/Program.cs
+++ PlayerPowers/Program.cs
@@ -4,9 +4,70 @@ namespace PlayerPowers
 {
     class Program
     {
+        [Flags]
+            enum Power
+            {
+                Fly = 1 << 0 , // 1
+                XRayVision = 1 << 1 , // 2
+                SuperStrength = 1 << 2, // 4
+            }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Power mypower;
+            
+            Console.Write("Quantos jogadores são: ");
+            string str = Console.ReadLine();
+            Console.WriteLine("");
+            
+            int njogadores = int.Parse(str);
+            Power [] jogadores = new Power[njogadores];
+
+            for(int i = 0; i < njogadores; i++)
+            {
+                Console.WriteLine("Lista de poderes:\nFly = 'f'\nXRayVision = 'x'\nSuperStrength = 's'");
+                Console.Write($"Que poderes tem o jogador {i+1}: ");
+                string answer = Console.ReadLine();    
+                
+                mypower = 0;
+
+                foreach(char s in answer)//carácter por carácter da lista de "answer"
+                {
+                    if (s == 'f')
+                    {
+                        mypower ^= Power.Fly;
+                    }
+                    else if (s == 'x')
+                    {
+                        mypower ^= Power.XRayVision;
+                    }
+                    else if (s == 's')
+                    {
+                        mypower ^= Power.SuperStrength;
+                    }
+                }
+
+                jogadores[i] = mypower;
+                Console.WriteLine("");
+            }
+
+            for(int i = 0; i < jogadores.Length; i++)
+            {
+                Console.Write($"O {i+1}º jogador tem os poderes {jogadores[i]}");
+
+                if((jogadores[i] & Power.SuperStrength) == Power.SuperStrength && (jogadores[i] & Power.Fly) == Power.Fly)
+                {
+                    Console.WriteLine(", Flying Radiation!");
+                }
+                else
+                {
+                    Console.WriteLine("!");
+                }
+
+                Console.WriteLine("");
+            }
+
+
+
         }
     }
 }
